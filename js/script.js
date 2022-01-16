39859f7eef905a5dc0dd0f0835a1400a5c176196
KondoweT <mukukakondowe@gmail.com>
2022-01-17 00:35:10 +0200

    refacted script.js and add html elements

--- js/script.js
+++ js/script.js
@@ -1,92 +1,92 @@
+const computerOption = ['Rock', 'Paper', 'Scissors'];
+let playerSelection, computerSelection;
+let playerScore = 0;
+let computerScore = 0;
+let playerScoreOut = document.querySelector('#playerScore');
+let computerScoreOut = document.querySelector('#computerScore');
+let message = document.querySelector('#message');
+let playerChoice = document.querySelector('#playerSelection');
+let computerChoice = document.querySelector('#computerSelection');
+let restart = document.querySelector('#restart');
+
 function computerPlay(){
-    let randomNum = Math.floor(Math.random() * (4 - 1)) + 1;
-    let computerChoice = '';
-    switch (randomNum) {
-        case 1:
-            computerChoice = 'Rock';
-            break;
-        case 2:
-            computerChoice = 'Paper';
-            break;
-        default:
-            computerChoice = 'Scissors';
-    }
-    return computerChoice;
+    return computerOption[Math.floor(Math.random() * (2 - 0 + 1) ) + 0];
 }
 
-function gamerPlay(id){
-    let playerChoice = id;
-        let lCase = playerChoice.toLowerCase();
-        let fLetterTUpperCase =lCase[0].toUpperCase();
-        let finalChoice = fLetterTUpperCase + lCase.slice(1);
+function playRound(playerSelection, computerSelection){
+    computerSelection = computerPlay();
+    if(playerScore !== 5 && computerScore < 5 || computerScore !== 5 && playerScore < 5){
+        if(playerSelection === 'Rock'&& computerSelection === 'Scissors'||
+           playerSelection === 'Scissors' && computerSelection === 'Paper'||
+           playerSelection === 'Paper' && computerSelection === 'Rock'){
 
-        if(finalChoice === 'Rock' || finalChoice === 'Scissors'|| finalChoice === 'Paper'){
-            return finalChoice;
-        }else{
-            return 'ERROR';
-        }
-    
-    
-}
+            playerChoice.textContent = playerSelection;
+            computerChoice.textContent = computerSelection;
+                playerScore += 1;
+               message.textContent = `You Win! ${playerSelection} beats ${computerSelection}`;
+        }else if(computerSelection === 'Rock'&& playerSelection === 'Scissors'||
+                 computerSelection === 'Scissors' && playerSelection === 'Paper'||
+                 computerSelection === 'Paper' && playerSelection === 'Rock'){
 
-function playRound(playerSelection, computerSelection) {
-    if(playerSelection === 'ERROR'){
-        return 'ERROR';
-    }else{
-        if(playerSelection === 'Rock' && computerSelection === 'Scissors'){
-            return `You Win! ${playerSelection} beats ${computerSelection}`;
-        }else if(playerSelection === 'Paper' && computerSelection === 'Rock'){
-            return `You Win! ${playerSelection} beats ${computerSelection}`;
-        }else if(playerSelection === 'Scissors' && computerSelection === 'Paper'){
-            return `You Win! ${playerSelection} beats ${computerSelection}`;
+                    playerChoice.textContent = playerSelection;
+                    computerChoice.textContent = computerSelection;
+            computerScore += 1;
+            message.textContent = `You Lose! ${computerSelection} beats ${playerSelection}`;
         }else if(playerSelection === computerSelection){
-            return `It's a Tie! ${playerSelection} is the same as ${computerSelection}`;
-        }else{
-            return `You Lose! ${computerSelection} beats ${playerSelection}`;
+
+            playerChoice.textContent = playerSelection;
+            computerChoice.textContent = computerSelection;
+            message.textContent = `It's a Tie! ${playerSelection} equals ${computerSelection}`;
+        }
+    }else if(Number(playerScoreOut.textContent) === 5 || Number(computerScoreOut.textContent) === 5){
+        if(playerScore ===5 && computerScore < playerScore){
+            message.textContent = `You Won! You are the First to 5 points.`;
+            message.style.color = 'green';
+        }else if(computerScore === 5 && playerScore < computerScore){
+            message.textContent = `You Lost! The Computer was First to 5 points.`;
+            message.style.color = 'red';
         }
     }
 }
 
-// function getWinner(playerScore, computerScore){
-//     if(playerScore > computerScore){
-//         return `You Won the Game! ${playerScore} to ${computerScore}`;
-//     }else if(computerScore > playerScore){
-//         return `You Lost the Game! ${computerScore} to ${playerScore}`;
-//     }else{
-//         return `You Drew the Game! ${computerScore} to ${playerScore}`;
-//     }
-// }
-
-// function game(){
-//     let playerScore = 0, computerScore = 0;
-//     for(let i = 0; i < 5; i++){
-//         const playerSelection = gamerPlay();
-//         const computerSelection = computerPlay();
-//         const round = playRound(playerSelection, computerSelection);
-//         if (round.indexOf('Win') > 0) {
-//             playerScore += 1;
-//             alert(`${round}\n playerScore: ${playerScore} computerScore: ${computerScore}`);
-//         } else if(round.indexOf('Lose') > 0){
-//             computerScore += 1;
-//             alert(`${round}\n playerScore: ${playerScore} computerScore: ${computerScore}`);
-//         }else{
-//             alert(`${round}\n playerScore: ${playerScore} computerScore: ${computerScore}`);
-//         }
-//     }
-//     const gameResult = getWinner(playerScore,computerScore);
-//     return gameResult;
-// }
-
-// let gamePlay = game();
-// alert(gamePlay);
-
-const btnOptions = document.querySelectorAll('.btnOption');
-btnOptions.forEach((btnOption)=>{
-    btnOption.addEventListener('click',()=>{
-        const id = btnOption.id;
-        const playerSelection = gamerPlay(id);
-        const computerSelection = computerPlay();
-        const roundWinner = playRound(playerSelection, computerSelection);
-        alert(roundWinner);
+if(Number(playerScoreOut.textContent) === 5 || Number(computerScoreOut.textContent) === 5){
+    if(playerScore ===5 && computerScore < playerScore){
+        message.textContent = `You Won! You are the First to 5 points.`;
+        message.style.color = 'green';
+    }else if(computerScore === 5 && playerScore < computerScore){
+        message.textContent = `You Lost! The Computer was First to 5 points.`;
+        message.style.color = 'red';
+    }
+}
+const playerOptions = document.querySelectorAll('[data-selection]');
+playerOptions.forEach(playerSelection =>{
+    playerSelection.addEventListener('click', playerSelection =>{
+        playerSelection = playerSelection.target.id;
+        if(Number(playerScoreOut.textContent) === 5 || Number(computerScoreOut.textContent) === 5){
+            if(playerScore ===5 && computerScore < playerScore){
+                message.textContent = `You Won! You are the First to 5 points.`;
+                message.style.color = 'green';
+            }else if(computerScore === 5 && playerScore < computerScore){
+                message.textContent = `You Lost! The Computer was First to 5 points.`;
+                message.style.color = 'red';
+            }
+        }else
+            playRound(playerSelection, computerSelection);
+        if(computerScore < playerScore && playerScore <= 5 ||
+            playerScore < computerScore && computerScore <= 5){
+            playerScoreOut.textContent = playerScore;
+            computerScoreOut.textContent = computerScore;
+        }
+        
     });
+});
+
+restart.addEventListener('click', ()=>{
+    playerScore = 0;
+    computerScore = 0;
+    playerScoreOut.textContent = 0, computerScoreOut.textContent = 0;
+    playerChoice.textContent = '';
+    computerChoice.textContent = '';
+    message.style.color = 'black';
+    message.textContent = 'Winner of round gets shown here';
 });
