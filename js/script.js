cbecdfab9b38e973f484a7963fadef7fb0e5ce89
KondoweT <mukukakondowe@gmail.com>
2022-01-19 00:54:53 +0200

    completed ui for rock paper scissors

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const computerOption = ['Rock', 'Paper', 'Scissors'];
+const playerOptions = document.querySelectorAll('[data-selection]');
 let playerSelection, computerSelection;
 let playerScore = 0;
 let computerScore = 0;
@@ -10,9 +11,10 @@ let computerChoice = document.querySelector('#computerSelection');
 let restart = document.querySelector('#restart');
 
 function computerPlay(){
-    return computerOption[Math.floor(Math.random() * (2 - 0 + 1) ) + 0];
+    return computerOption[Math.floor(Math.random() * 3)];
 }
 
+// playround plays the role of game() as well
 function playRound(playerSelection, computerSelection){
     computerSelection = computerPlay();
     if(playerScore !== 5 && computerScore < 5 || computerScore !== 5 && playerScore < 5){
@@ -20,58 +22,41 @@ function playRound(playerSelection, computerSelection){
            playerSelection === 'Scissors' && computerSelection === 'Paper'||
            playerSelection === 'Paper' && computerSelection === 'Rock'){
 
+            playerScore += 1;
             playerChoice.textContent = playerSelection;
             computerChoice.textContent = computerSelection;
-                playerScore += 1;
-               message.textContent = `You Win! ${playerSelection} beats ${computerSelection}`;
+                if(playerScore ===5 && computerScore < playerScore){
+                    message.textContent = `You Won! You are the First to 5 points.`;
+                    message.style.color = 'green';
+                }else
+                    message.textContent = `You Win! ${playerSelection} beats ${computerSelection}`;
         }else if(computerSelection === 'Rock'&& playerSelection === 'Scissors'||
                  computerSelection === 'Scissors' && playerSelection === 'Paper'||
                  computerSelection === 'Paper' && playerSelection === 'Rock'){
 
+                    computerScore += 1;
                     playerChoice.textContent = playerSelection;
                     computerChoice.textContent = computerSelection;
-            computerScore += 1;
-            message.textContent = `You Lose! ${computerSelection} beats ${playerSelection}`;
+            
+            if(computerScore === 5 && playerScore < computerScore){
+                message.textContent = `You Lost! The Computer was First to 5 points.`;
+                message.style.color = 'red';
+            }else
+                message.textContent = `You Lose! ${computerSelection} beats ${playerSelection}`;
         }else if(playerSelection === computerSelection){
 
             playerChoice.textContent = playerSelection;
             computerChoice.textContent = computerSelection;
             message.textContent = `It's a Tie! ${playerSelection} equals ${computerSelection}`;
         }
-    }else if(Number(playerScoreOut.textContent) === 5 || Number(computerScoreOut.textContent) === 5){
-        if(playerScore ===5 && computerScore < playerScore){
-            message.textContent = `You Won! You are the First to 5 points.`;
-            message.style.color = 'green';
-        }else if(computerScore === 5 && playerScore < computerScore){
-            message.textContent = `You Lost! The Computer was First to 5 points.`;
-            message.style.color = 'red';
-        }
     }
 }
 
-if(Number(playerScoreOut.textContent) === 5 || Number(computerScoreOut.textContent) === 5){
-    if(playerScore ===5 && computerScore < playerScore){
-        message.textContent = `You Won! You are the First to 5 points.`;
-        message.style.color = 'green';
-    }else if(computerScore === 5 && playerScore < computerScore){
-        message.textContent = `You Lost! The Computer was First to 5 points.`;
-        message.style.color = 'red';
-    }
-}
-const playerOptions = document.querySelectorAll('[data-selection]');
+// Adds event listener to playing buttons
 playerOptions.forEach(playerSelection =>{
     playerSelection.addEventListener('click', playerSelection =>{
         playerSelection = playerSelection.target.id;
-        if(Number(playerScoreOut.textContent) === 5 || Number(computerScoreOut.textContent) === 5){
-            if(playerScore ===5 && computerScore < playerScore){
-                message.textContent = `You Won! You are the First to 5 points.`;
-                message.style.color = 'green';
-            }else if(computerScore === 5 && playerScore < computerScore){
-                message.textContent = `You Lost! The Computer was First to 5 points.`;
-                message.style.color = 'red';
-            }
-        }else
-            playRound(playerSelection, computerSelection);
+        playRound(playerSelection, computerSelection);
         if(computerScore < playerScore && playerScore <= 5 ||
             playerScore < computerScore && computerScore <= 5){
             playerScoreOut.textContent = playerScore;
@@ -81,12 +66,13 @@ playerOptions.forEach(playerSelection =>{
     });
 });
 
+// Resets the score and restarts game
 restart.addEventListener('click', ()=>{
     playerScore = 0;
     computerScore = 0;
     playerScoreOut.textContent = 0, computerScoreOut.textContent = 0;
-    playerChoice.textContent = '';
-    computerChoice.textContent = '';
+    playerChoice.textContent = '❔';
+    computerChoice.textContent = '❔';
     message.style.color = 'black';
     message.textContent = 'Winner of round gets shown here';
 });
